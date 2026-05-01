6f43911dd6cd2aeca2a64dd45698bdc2e41c632a
marc <marcandreinisperos13@gmail.com>
2026-05-02 03:54:15 +0800

    feat: created initial UI for Home Page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom'
 
 export default function Home() {
   return (
-    <div>
-      <h1>Welcome to the Teyvat Restaurant</h1>
-      <h3></h3>
-      <Link to="/menu">Go to Menu</Link>
+    <div className='min-h-screen center-flex flex-col'>
+      <h1 className='font-bold text-5xl text-[var(--secondary-color)]'>Welcome to the Teyvat Restaurant</h1>
+      <h3 className='text-xl'>Enjoy your meal!</h3>
+      <Link to="/menu" className='button mt-10 px-6 py-2 transition'>Go to Menu</Link>
     </div>
   )
 }
