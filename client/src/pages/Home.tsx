d629dd4e1018da456ef4f685cb30a6b805ae5fbe
marc <marcandreinisperos13@gmail.com>
2026-05-03 03:28:42 +0800

    feat: added home bg image and some changes to the text in home page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom'
 
 export default function Home() {
   return (
-    <div className='min-h-screen center-flex flex-col'>
-      <h1 className='font-bold text-5xl text-[var(--secondary-color)]'>Welcome to the Teyvat Restaurant</h1>
-      <h3 className='text-xl'>Enjoy your meal!</h3>
+    <div className='min-h-screen center-flex flex-col bg-[linear-gradient(rgba(0,0,0,0.4),rgba(0,0,0,0.4)),url("/src/assets/home-bg.jpg")] bg-cover bg-center'>
+      <h1 className='font-bold text-5xl text-[var(--secondary-color)]'>Welcome to Teyvat Restaurant</h1>
+      <h3 className='text-xl text-[var(--secondary-text-color)]'>A world of flavors awaits you</h3>
       <Link to="/menu" className='button mt-10 px-6 py-2 transition'>Go to Menu</Link>
     </div>
   )
