60f46902fc2eb004e271e2c70082f4d8e5a694a8
marc <marcandreinisperos13@gmail.com>
2026-05-03 06:25:09 +0800

    some changes to home page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -5,8 +5,8 @@ export default function Home() {
   return (
     <div className='min-h-screen center-flex flex-col bg-[linear-gradient(rgba(0,0,0,0.4),rgba(0,0,0,0.4)),url("/src/assets/home-bg.jpg")] bg-cover bg-center'>
       <h1 className='font-bold text-5xl text-[var(--secondary-color)]'>Welcome to Teyvat Restaurant</h1>
-      <h3 className='text-xl text-[var(--secondary-text-color)]'>A world of flavors awaits you</h3>
-      <Link to="/menu" className='button mt-10 px-6 py-2 transition'>Go to Menu</Link>
+      <h3 className='mb-10 mt-3 text-xl text-[var(--secondary-text-color)]'>A world of flavors awaits you</h3>
+      <Link to="/menu" className='button px-6 py-2 transition'>Go to Menu</Link>
     </div>
   )
 }
